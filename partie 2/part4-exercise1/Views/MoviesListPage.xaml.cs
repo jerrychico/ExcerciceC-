283032c09914ee7bae7a507139b9f51a92119402
Jerry107 <jerrychico42@gmail.com>
2026-04-17 14:22:17 -0300

    Ajout du .gitignore et nettoyage des fichiers de build

---
+++ partie 2/part4-exercise1/Views/MoviesListPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieCatalog.Views;
+
+public partial class MoviesListPage : ContentPage
+{
+	public MoviesListPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+{
+    if (e.CurrentSelection.Count == 0)
+        return;
+
+    await Navigation.PushAsync(new Views.MovieDetailPage());
+
+    // Deselect
+    ((CollectionView)sender).SelectedItem = null;
+}
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        MenuItem menuItem = (MenuItem)sender;
+        ViewModels.MovieViewModel movie = (ViewModels.MovieViewModel)menuItem.BindingContext;
+        App.MainViewModel?.DeleteMovie(movie);
+    }
+}
